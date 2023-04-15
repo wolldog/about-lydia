33e688aa817067d846089468292878636d4582b7
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-15 15:23:50 +1000

    M. App.js; import cmpnts, add currentpage function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React, { useState } from 'react';
+import About from './components/pages/About';
+import Contact from './components/pages/Contact';
+import Resume from './components/pages/Resume';
+import Portfolio from './components/pages/Portfolio';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
+//Variable to hold the currently rendered page and default 'About'
+const [currentPage, setCurrentPage] = useState('About');
+
+// This method checks the value of `currentPage`and returns the appropriate component.
+const renderPage = () => {
+  if(currentPage === 'About') {
+    return <About />;
+  }
+  if(currentPage === 'Portfolio') {
+    return <Portfolio />;
+  }
+  if(currentPage === 'Resume') {
+    return <Resume />;
+  }
+    return <Contact />    
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div>
-      <Header />
+      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
       <main>
         {renderPage()}
       </main>
