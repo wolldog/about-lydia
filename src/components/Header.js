41527959df13c6524e65b745229bb2748b7c6aae
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-15 16:04:47 +1000

    M. Header.js; Add links, check handlePageChange

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-export default function Header() {
+
+export default function Header({currentPage, handlePageChange}) {
+
     return (
         <div>
             <h1>Header</h1>
+            <ul>
+             <li><a href='#About' onClick={() => handlePageChange('About')}>About</a></li>
+             <li><a href='#Portfolio' onClick={() => handlePageChange('Portfolio')}>Portfolio</a></li>
+             <li><a href='#Contact' onClick={() => handlePageChange('Contact')}>Contact</a></li>
+             <li><a href='#Resume' onClick={() => handlePageChange('Resume')}>Resume</a></li>
+            </ul>
         </div>
     );
-}
+};
