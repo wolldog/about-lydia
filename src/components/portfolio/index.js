38ec2545ce469f8a8b3707f7e6081c810ba9d4d1
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-17 21:39:43 +1000

    M. portfolio/index.js; add bootstrap & map content

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { Projects } from './Projects';
+import React from "react";
+import { Projects } from "./Projects";
+import Card from "react-bootstrap/Card";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
 
 export default function Portfolio() {
-    return (
-        <div>
-            <h1>Portfolio</h1>
-                <ul>
-                    {Projects.map((project, index)=>{
-                        return (
-                            <li key={index}>
-                                <a href={project.github}> {project.title} </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Portfolio</h1>
+
+      {Projects.map((project, index) => {
+        return (
+          <Row xs={1} md={2} className="g-4">
+            <Col>
+              <Card>
+                <Card.Img variant="top" src={require('../../assets/image/' + project.image + '.jpg')} />
+                <Card.Body>
+                  <Card.Title>{project.title}</Card.Title>
+                  <Card.Text>
+                    This is a longer card with supporting text below as a
+                    natural lead-in to additional content. This content is a
+                    little bit longer.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
+        );
+      })}
+    </div>
+  );
 }
