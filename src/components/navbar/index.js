c69a97fce9fa7f7307592dcb4710d6f8b86f99ca
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-21 17:22:17 +1000

    M. navbar/index.js; add navigation props and CSS

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,28 +1,64 @@
 import React from "react";
-import Nav from 'react-bootstrap/Nav';
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 const styles = {
-    link: {
-        fontSize: '1.2rem',
-    }
-}
+  links: {
+    fontSize: "larger",
+    marginLeft: "30px",
+    marginRight: "30px",
+  },
 
-export default function Navbar(props) {
-    return (
-            <Nav className="justify-content-end">
-             <Nav.Item>
-                <Nav.Link style={styles.link} href="#About" onClick={() => props.handlePageChange('About')}>About</Nav.Link>
-             </Nav.Item>
-             <Nav.Item>
-                <Nav.Link style={styles.link} href="#Portfolio" onClick={() => props.handlePageChange('Portfolio')}>Portfolio</Nav.Link>
-             </Nav.Item>
-             <Nav.Item>
-                <Nav.Link style={styles.link} href="#Contact" onClick={() => props.handlePageChange('Contact')}>Contact</Nav.Link>
-             </Nav.Item>
-             <Nav.Item>
-                <Nav.Link style={styles.link} href="#Resume" onClick={() => props.handlePageChange('Resume')}>Resume</Nav.Link>
-             </Nav.Item>
-             </Nav>
-     
-    )
+  brand: {
+    fontSize: "40px",
+    marginLeft: "20px",
+  },
+};
+
+export default function Navigation(props) {
+  return (
+    <Navbar expand="md">
+      <Navbar.Brand style={styles.brand} href="#About">
+        LydiaWallis
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+        <Nav>
+          <Nav.Link
+            style={styles.links}
+            href="#About"
+            onClick={() => props.handlePageChange("About")}
+          >
+            About
+          </Nav.Link>
+
+          <Nav.Link
+            style={styles.links}
+            href="#Portfolio"
+            onClick={() => props.handlePageChange("Portfolio")}
+          >
+            Portfolio
+          </Nav.Link>
+
+          <Nav.Link
+            style={styles.links}
+            href="#Contact"
+            onClick={() => props.handlePageChange("Contact")}
+          >
+            Contact
+          </Nav.Link>
+
+          <Nav.Link
+            style={styles.links}
+            href="#Resume"
+            onClick={() => props.handlePageChange("Resume")}
+          >
+            Resume
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+
+      {/* </Container> */}
+    </Navbar>
+  );
 }
