2c9615a13c6b1360b0dcf5e9684b29e0e6444318
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-21 17:25:31 +1000

    M. Header comp.; add Navbar comp & CSS

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,19 @@
-import React from 'react';
-import Navbar from './navbar';
-import Container from 'react-bootstrap/Container';
+import React from "react";
+import Navigation from "./navbar";
 
 const styles = {
-    background: {
-        background: '#f5f5f5',
-        padding: '20px',
-        opacity: '0.9'
-        
-    }
-}
+  background: {
+    background: "#f5f5f5",
+    opacity: "0.8",
+    display: "flex-box",
+    width: "100%",
+  },
+};
+
 export default function Header(props) {
-    return (
-        <div style={styles.background}>
-            <Container fluid>
-                <Navbar handlePageChange={props.handlePageChange} />
-            </Container>
-        </div>
-    );
+  return (
+    <div style={styles.background}>
+      <Navigation handlePageChange={props.handlePageChange} />
+    </div>
+  );
 }
