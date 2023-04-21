9810eb6bd0ff93b8e934d1c28038ffcbc076a380
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-21 15:31:15 +1000

    M portfolio/index.js; add overlay and CSS

--- src/components/portfolio/index.js
+++ src/components/portfolio/index.js
@@ -3,31 +3,52 @@ import { Projects } from "./Projects";
 import Card from "react-bootstrap/Card";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import "./portfolio.css";
+import { Container } from "react-bootstrap";
+import github from "../../assets/icons/github.svg";
+
+const styles = {
+  icon: {
+    height: "40px",
+  },
+};
 
 export default function Portfolio() {
   return (
     <div>
-      <h1>Portfolio</h1>
-
-      {Projects.map((project, index) => {
-        return (
-          <Row xs={1} md={2} className="g-4">
-            <Col>
-              <Card>
-                <Card.Img variant="top" src={require('../../assets/image/' + project.image + '.jpg')} />
-                <Card.Body>
-                  <Card.Title>{project.title}</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        );
-      })}
+      <Row>
+        <Col>
+          <h1 className="heading"> Portfolio </h1>
+        </Col>
+      </Row>
+      <Container className="pageContent">
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {Projects.map((project, index) => {
+            return (
+              <Col>
+                <Card className="card">
+                  <Card.Img
+                    src={require("../../assets/image/" +
+                      project.image +
+                      ".png")}
+                    className="image"
+                    key={index}
+                  />
+                  <div className="overlay">
+                    <a className="projectLink" href={project.deployedLink}>
+                      <h3> {project.title}</h3>
+                    </a>
+                    <h4 className="techs">{project.tech}</h4>
+                    <a href={project.github}>
+                      <img style={styles.icon} src={github} />
+                    </a>
+                  </div>
+                </Card>
+              </Col>
+            );
+          })}
+        </Row>
+      </Container>
     </div>
   );
 }
