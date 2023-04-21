9630e32bb06b2474a3a2bf301b0e998460d73d31
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-21 17:23:43 +1000

    M. contact/index.js: add form elements & functions

--- src/components/contact/index.js
+++ src/components/contact/index.js
@@ -1,77 +1,118 @@
-import React from 'react';
-import { Button, Form, Input } from 'antd';
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./contact.css";
 
+//Import a helper function that will check if the email is valid
+import { validateEmail } from "../../utils/helpers";
 
-const layout = {
-  labelCol: {
-    span: 8,
-  },
-  wrapperCol: {
-    span: 16,
-  },
-};
+export default function Contact() {
+  
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-/* eslint-disable no-template-curly-in-string */
-const validateMessages = {
-  required: '${label} is required!',
-  types: {
-    email: '${label} is not a valid email!',
-  },
-};
-/* eslint-enable no-template-curly-in-string */
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-const onFinish = (values) => {
-  console.log(values);
-};
+    // Based on the input type, we set the state of either email, username, and password
+    if (inputType === "name") {
+      setName(inputValue);
+    } else if (inputType === "email") {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
 
-const Contact = () => (
-  <Form
-    {...layout}
-    name="nest-messages"
-    onFinish={onFinish}
-    style={{
-      maxWidth: 600,
-    }}
-    validateMessages={validateMessages}
-  >
-    <Form.Item
-      name={['user', 'name']}
-      label="Name"
-      rules={[
-        {
-          required: true,
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
+  const handleBlur = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+
+    if (target.value.length === 0) {
+      setErrorMessage(`Your ${inputType} is required to submit`);
+    }
     
-    <Form.Item
-      name={['user', 'email']}
-      label="Email"
-      rules={[
-        {
-          type: 'email',
-        },
-      ]}
-    >
-      <Input />
-    </Form.Item>
+  };
 
-    <Form.Item name={['user', 'introduction']} label="Introduction">
-      <Input.TextArea />
-    </Form.Item>
-    <Form.Item
-      wrapperCol={{
-        ...layout.wrapperCol,
-        offset: 8,
-      }}
-    >
-      <Button type="primary" htmlType="submit">
-        Submit
-      </Button>
-    </Form.Item>
-  </Form>
-);
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of page refresh on submit
+    e.preventDefault();
 
-export default Contact
+    // Validate email address. If invalid display error message.
+    
+    if (!validateEmail(email)) {
+      setErrorMessage("Please enter a valid email address");
+      // Exit out of this code block so that the user can correct it
+      return;
+    }
+
+    // Confirm successful submission
+    alert(`Hello ${name}! Thank you for your message`);
+
+    // Clear input fields if form submit is successful
+    setName("");
+    setEmail("");
+    setMessage("");
+  };
+
+  return (
+
+    <div>
+      <h1 className="heading">Contact</h1>
+         {errorMessage && (
+      <div>
+        <p className="error">{errorMessage}</p>
+      </div>
+    )}
+    
+      <Form className="formContainer" style={{width:'75%', margin:'auto'}}>
+        <Form.Group className="mb-4">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="text"
+            name="name"
+            placeholder="Enter your name"
+            onBlur={handleBlur}
+            onChange={handleInputChange}
+            value={name}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-4">
+          <Form.Label>Email</Form.Label>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter your email address"
+            onBlur={handleBlur}
+            onChange={handleInputChange}
+            value={email}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-4">
+          <Form.Label>Message</Form.Label>
+          <Form.Control
+            as="textarea"
+            type="text-area"
+            name="message"
+            placeholder="Enter your message"
+            onBlur={handleBlur}
+            onChange={handleInputChange}
+            value={message}
+          />
+        </Form.Group>
+
+        <Button variant="secondary" type="submit" onClick={handleFormSubmit}>
+          Submit
+        </Button>
+      </Form>
+
+    </div>
+  );
+}
