b72b49c216330c453a28433adbe656aec5d285d8
Woll Dog <110208272+wolldog@users.noreply.github.com>
2023-04-21 17:27:43 +1000

    M App.js; minor adjustments

--- src/App.js
+++ src/App.js
@@ -1,39 +1,37 @@
-import React, { useState } from 'react';
-import About from './components/about';
-import Contact from './components/contact';
-import Resume from './components/resume';
-import Portfolio from './components/portfolio';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
+import React, { useState } from "react";
+import About from "./components/about";
+import Contact from "./components/contact";
+import Resume from "./components/resume";
+import Portfolio from "./components/portfolio";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import "./App.css";
 
 function App() {
+  
+  //Variable to hold the currently rendered page and default 'About'
+  const [currentPage, setCurrentPage] = useState("About");
 
-//Variable to hold the currently rendered page and default 'About'
-const [currentPage, setCurrentPage] = useState('About');
-
-// This method checks the value of `currentPage`and returns the appropriate component.
-const renderPage = () => {
-  if(currentPage === 'About') {
-    return <About />;
-  }
-  if(currentPage === 'Portfolio') {
-    return <Portfolio />;
-  }
-  if(currentPage === 'Resume') {
-    return <Resume />;
-  }
-    return <Contact />    
+  // This method checks the value of `currentPage`and returns the appropriate component.
+  const renderPage = () => {
+    if (currentPage === "About") {
+      return <About />;
+    }
+    if (currentPage === "Portfolio") {
+      return <Portfolio />;
+    }
+    if (currentPage === "Resume") {
+      return <Resume />;
+    }
+    return <Contact />;
   };
 
   const handlePageChange = (page) => setCurrentPage(page);
 
   return (
     <div>
-      <Header currentPage={currentPage} handlePageChange={handlePageChange} />
-      <main>
-        {renderPage()}
-      </main>
+      <Header handlePageChange={handlePageChange} />
+      <main className="pageContainer">{renderPage()}</main>
       <Footer />
     </div>
   );
